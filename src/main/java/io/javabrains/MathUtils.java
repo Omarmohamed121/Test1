ce34b552b91f2711917923a87cf6e035181da791
omar <omar00@localhost.localdomain>
2025-10-19 23:12:49 +0300

    Initial commit

---
+++ src/main/java/io/javabrains/MathUtils.java
@@ -0,0 +1,6 @@
+package io.javabrains;
+public class MathUtils {
+    public int add(int a , int b){
+        return a + b ;
+    }
+}
---
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package io.javabrains;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class MathUtilsTest {
+    @Test
+    void test(){
+        MathUtils math = new MathUtils();
+        int expected = 2 ;
+        int actual = math.add(1,1);
+
+        assertEquals(expected,actual);
+    }
+}
